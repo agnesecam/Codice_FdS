c488485c9b1c509caecd40875243c52055f45a7d
Agnese <agnesecam@hotmail.it>
2022-05-27 16:16:59 +0200

    Creazione repository

---
+++ FdS2.js
@@ -0,0 +1,23 @@
+function testOne(){
+    SaxonJS.transform({
+        stylesheetLocation: "FdS.sef.json",
+        sourceLocation: "MarcaturaProlusioni.xml"
+    }, "async")
+    /*SaxonJS.transform({
+        stylesheetLocation: "FdStext.sef.json",
+        sourceLocation: "MarcaturaProlusioni.xml"
+    }, "async")*/
+}
+
+/*
+window.onload = function testOne(){
+    SaxonJS.transform({
+        stylesheetLocation: "FdS.sef.json",
+        sourceLocation: "MarcaturaProlusioni.xml"
+    }, "async")
+    /*SaxonJS.transform({
+        stylesheetLocation: "FdStext.sef.json",
+        sourceLocation: "MarcaturaProlusioni.xml"
+    }, "async")
+}
+*/
