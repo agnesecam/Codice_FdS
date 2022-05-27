8b30e19e78f6adb9fec28b6173c87932b632e9f2
Agnese <agnesecam@hotmail.it>
2022-05-27 16:47:18 +0200

    Tolto onload nell'HTML e inserito window.onload

--- JavaScript/FdS.js
+++ JavaScript/FdS.js
@@ -1,23 +1,5 @@
-function testOne(){
+window.onload = function testOne(){
     SaxonJS.transform({
         stylesheetLocation: "../FdS.sef.json",
         sourceLocation: "../XML/MarcaturaProlusioni.xml"
-    }, "async")
-    /*SaxonJS.transform({
-        stylesheetLocation: "FdStext.sef.json",
-        sourceLocation: "MarcaturaProlusioni.xml"
-    }, "async")*/
-}
-
-/*
-window.onload = function testOne(){
-    SaxonJS.transform({
-        stylesheetLocation: "FdS.sef.json",
-        sourceLocation: "MarcaturaProlusioni.xml"
-    }, "async")
-    /*SaxonJS.transform({
-        stylesheetLocation: "FdStext.sef.json",
-        sourceLocation: "MarcaturaProlusioni.xml"
-    }, "async")
-}
-*/
+    }, "async")}
