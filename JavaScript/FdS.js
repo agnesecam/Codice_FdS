897ec59a90ff4ccf809270fd9ceac8f6c2363248
Agnese <agnesecam@hotmail.it>
2022-10-07 15:47:55 +0200

    Lavori su testo p 11

--- JavaScript/FdS.js
+++ JavaScript/FdS.js
@@ -2,7 +2,7 @@
 window.onload = function testOne(){
     SaxonJS.transform({
         stylesheetLocation: "../FdS.sef.json",
-        sourceLocation: "XML/Prolusioni1_10_included.xml"
+        sourceLocation: "XML/Prolusioni1_11_included.xml"
     }, "async")
 }
 
