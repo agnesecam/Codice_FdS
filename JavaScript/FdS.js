9dff21049533f7fb146310e4ae6d100f2a92545d
Agnese <agnesecam@hotmail.it>
2022-10-20 10:15:53 +0200

    Cambio URL per bibl articolo Horace Micheli usato da Chidichimo

--- JavaScript/FdS.js
+++ JavaScript/FdS.js
@@ -2,7 +2,7 @@
 window.onload = function testOne(){
     SaxonJS.transform({
         stylesheetLocation: "../FdS.sef.json",
-        sourceLocation: "XML/Prolusioni1_30_included.xml"
+        sourceLocation: "XML/Prolusioni1_1_included.xml"
     }, "async")
 }
 
